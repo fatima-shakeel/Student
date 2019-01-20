c54af3c8189fa58bda8a9659ebad3e45d68b5ab5
Fatima Shakeel <fatimashakeel98@hotmail.com>
2019-01-20 18:31:07 +0500

    Create and list of students

---
+++ FinalAssesment/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalAssesment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalAssesment.Controllers
+{
+    public class StudentController : Controller
+    {
+        private studentContext _ORM;
+        public StudentController(studentContext ORM)
+        {
+            _ORM = ORM;
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student S)
+        {
+            _ORM.Student.Add(S);
+            _ORM.SaveChanges();
+            return View();
+        }
+
+        public IActionResult AllStudents()
+        {
+            return View();
+        }
+    }
+}
